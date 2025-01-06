9d00981d530b74d18bef52c71ac4d7d9cb84ef0d
Dinesh Kumar <mapmydestination@gmail.com>
2025-01-06 16:00:24 +0530

    update

--- vertical-timeline.php
+++ vertical-timeline.php
@@ -1,12 +1,9 @@
 <?php
-
 $connect = new PDO("mysql:host=localhost;dbname=testing", "root", "");
-
 $query = "SELECT * FROM timeline ORDER BY id ASC";
 $statement = $connect->prepare($query);
 $statement->execute();
 $result = $statement->fetchAll();
-
 ?>
 
 <html>  
